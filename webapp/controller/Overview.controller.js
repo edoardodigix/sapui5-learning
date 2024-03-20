7785228c6c7f5455a8a52bc4f2f20829ed3484ae
Edoardo Grasso <edoardo.grasso@digixplus.it>
2024-03-20 10:40:12 +0000

    implemented models

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/syncStyleClass"
+    "sap/ui/core/syncStyleClass",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, syncStyleClass) {
+    function (Controller, syncStyleClass, JSONModel) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Overview", {
@@ -30,6 +31,22 @@ sap.ui.define([
                 // note: You don't need to chain to the pDialog promise, since this event handler
                 // is only called from within the loaded dialog itself.
                 this.byId("dialog").close();
+            },
+
+            onInit: function () {
+                let oModel = new JSONModel();
+
+                // Passing data as JS Object tree to the model:
+                // oModel.setData({
+                //     CustomerName: "SAP SE",
+                //     City: "Walldorf"
+                // })
+                // Loading data from json file:
+                oModel.loadData("model/data.json");
+
+                // Assign the model to a view:
+                let oView = this.getView();
+                oView.setModel(oModel, "customer"); // Model to be set, model name
             }
 
         });
